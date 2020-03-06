94272e3cdcf43b727770a4510608b213506a8c5f
Jace Yang <magician@brandeis.edu>
2020-03-06 16:48:58 -0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
+  root 'users#signup'
+  
   resources :models
   resources :enrollments
   resources :instructors
   resources :courses
   resources :subjects
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'subjects#index'
-  root 'users#signup'
+  
+  get 'sessions/new'
   get  'course_list',    to: 'courses#index'
   get  'subject_list',    to: 'subjects#index'
   get  'instructor_list',    to: 'instructors#index'
